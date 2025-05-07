00aab473e5b146c002c45cf1190c9db856024a0b
runardvl <runarshakirov@gmail.com>
2025-05-07 12:21:25 +0300

    chor(config): Удаляет модуль nuxt-studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/content",
-    "@nuxthq/studio",
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/content"],
 
   routeRules: {
     // prerender index route by default
